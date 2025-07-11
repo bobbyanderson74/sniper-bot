256a09a3dc5924589880f80e1fd9e2d75c6a77cf
bobbyanderson74 <lhattar3@icloud.com>
2025-07-11 19:00:12 -0400

    Update token_scanner.py

--- token_scanner.py
+++ token_scanner.py
@@ -1,16 +1,17 @@
 import requests
 from config import CONFIG
 
-BIRDEYE_HEADERS = {
-    "X-API-KEY": CONFIG["61bd77e007e04b42b9dc95dc842bf583"]
-}
+def get_verified_tokens():
+    url = "https://public-api.birdeye.so/public/tokenlist?sort_by=created_at"
+    headers = {"X-API-KEY": CONFIG["birdeye_api_key"]}
 
-def get_new_tokens():
-    url = "https://public-api.birdeye.so/public/tokenlist?sort_by=volume_24h"
     try:
-        response = requests.get(url, headers=BIRDEYE_HEADERS)
-        data = response.json()
-        tokens = data.get("data", [])
-        return [token for token in tokens if token.get("liquidity", 0) >= CONFIG["min_liquidity"]]
+        res = requests.get(url, headers=headers)
+        data = res.json()
+        return [
+            {"name": t["name"], "address": t["address"], "liquidity": t.get("liquidity", 0)}
+            for t in data.get("data", [])
+            if t.get("liquidity", 0) >= CONFIG["min_liquidity"]
+        ]
     except:
         return []
