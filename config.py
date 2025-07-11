32447730b4f6ea07eabb0f76bc22b0f370f86fe0
bobbyanderson74 <lhattar3@icloud.com>
2025-07-11 18:50:14 -0400

    onfig.py

---
+++ config.py
@@ -0,0 +1,11 @@
+CONFIG = {
+    "phantom_private_key": "[154,2,210,50,105,168,7,103,163,133,123,58,126,215,58,229,14,232,171,106,72,191,115,74,62,21,225,91,129,167,64,217,212,133,90,196,79,65,27,19,194,58,159,60,182,223,239,140,164,171,97,173,211,141,101,124,183,65,66,215,52,24,30,97]",
+    "telegram_bot_token": "8101850489:AAH7gz08kHK184BBiBjukdiHAKVWw0B573s",
+    "telegram_chat_id": "7672755428",
+    "birdeye_api_key": "61bd77e007e04b42b9dc95dc842bf583",
+    "buy_amount_sol": 0.2,
+    "sell_multiplier": 2.0,
+    "dynamic_selling": True,
+    "price_check_delay": 12,
+    "min_liquidity": 5000
+}
